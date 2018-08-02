e0e7f6a867ecacd5de8d730c13fadfb858072845
RamilsonPraca <ramilsonpraca@gmail.com>
2018-08-01 21:27:11 -0300

    Revisão na classe Funcionario

--- Lanchonete/src/lanchonete/Funcionario.java
+++ Lanchonete/src/lanchonete/Funcionario.java
@@ -11,15 +11,16 @@ import java.util.Objects;
 public class Funcionario {
     
     private String nome;
-    private String rg;
+    private String cpf;
     private LocalDate nascimento;
     private String emailAcesso;
     private String senhaAcesso;
     private String telefone;
+    private String setor;
 
-    public Funcionario(String nome, String rg, LocalDate nascimento, String emailAcesso, String senhaAcesso, String telefone) {
+    public Funcionario(String nome, String cpf, LocalDate nascimento, String emailAcesso, String senhaAcesso, String telefone) {
         this.nome = nome;
-        this.rg = rg;
+        this.cpf = cpf;
         this.nascimento = nascimento;
         this.emailAcesso = emailAcesso;
         this.senhaAcesso = senhaAcesso;
@@ -34,12 +35,12 @@ public class Funcionario {
         this.nome = nome;
     }
 
-    public String getRg() {
-        return rg;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setRg(String rg) {
-        this.rg = rg;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public LocalDate getNascimento() {
@@ -73,16 +74,25 @@ public class Funcionario {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+    
+    public String getSetor(){
+        return setor;
+    }
+    
+    public void setSetor(String setor){
+        this.setor = setor;
+    }
 
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 41 * hash + Objects.hashCode(this.nome);
-        hash = 41 * hash + Objects.hashCode(this.rg);
+        hash = 41 * hash + Objects.hashCode(this.cpf);
         hash = 41 * hash + Objects.hashCode(this.nascimento);
         hash = 41 * hash + Objects.hashCode(this.emailAcesso);
         hash = 41 * hash + Objects.hashCode(this.senhaAcesso);
         hash = 41 * hash + Objects.hashCode(this.telefone);
+        hash = 41 * hash + Objects.hashCode(this.setor);
         return hash;
     }
 
@@ -101,7 +111,7 @@ public class Funcionario {
         if (!Objects.equals(this.nome, other.nome)) {
             return false;
         }
-        if (!Objects.equals(this.rg, other.rg)) {
+        if (!Objects.equals(this.cpf, other.cpf)) {
             return false;
         }
         if (!Objects.equals(this.emailAcesso, other.emailAcesso)) {
@@ -116,12 +126,15 @@ public class Funcionario {
         if (!Objects.equals(this.nascimento, other.nascimento)) {
             return false;
         }
+        if (!Objects.equals(this.setor, other.setor)) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public String toString() {
-        return "Funcionario{" + "nome=" + nome + ", rg=" + rg + ", nascimento=" + nascimento + ", emailAcesso=" + emailAcesso + ", senhaAcesso=" + senhaAcesso + ", telefone=" + telefone + '}';
+        return "Funcionario{" + "nome=" + nome + ", cpf=" + cpf + ", nascimento=" + nascimento + ", emailAcesso=" + emailAcesso + ", senhaAcesso=" + senhaAcesso + ", telefone=" + telefone + ", setor=" + setor +'}';
     }
     
     
